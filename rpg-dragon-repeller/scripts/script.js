878b165aa1e72d4a0b93110b2cc5b3d65a0e725d
Anirban Bhattacharya <10656131+obhi1kenobhi@users.noreply.github.com>
2024-03-15 20:26:14 +0530

    added functions for buttons and initialized the values

--- rpg-dragon-repeller/scripts/script.js
+++ rpg-dragon-repeller/scripts/script.js
@@ -1,14 +1,73 @@
 // //Variable declarations are of three types - var, const and let. 'let' is the most popular and shall be used here.
-let xp = 0;  // Declare a variable called xp. Initialize your xp variable to have a value of 0, a number.
-let health = 100; // Initialize another variable called health with a value of 100
-let gold = 50; // Initialize another variable called gold with a value of 50.
-let currentWeapon = 0; // Create another variable called currentWeapon and set it to 0.
-let fighting; // Declare a variable called fighting but do not initialize it with a value.
-let monsterHealth; // Declare a variable called monsterHealth but do not initialize it with a value.
-let inventory = ["stick"]; // Declare an array variable and initialize it to only one element, stick for now.
+let xp = 0;  
+let health = 100; 
+let gold = 50;
+let currentWeapon = 0; 
+let fighting;  
+let monsterHealth; 
+let inventory = ["stick"]; 
 
-// //The querySelector() method takes a CSS selector as an argument and returns the first element that matches that selector.
-const button1 = document.querySelector("#button1"); // Create a button1 constant and use querySelector() to assign it your element with the id of button1.
-const button2 = document.querySelector("#button2"); // Create a button2 constant and use querySelector() to assign it your element with the id of button2.
-const button3 = document.querySelector("#button3"); // Create a button3 constant and use querySelector() to assign it your element with the id of button3.
+// //The querySelector() method takes a CSS selector as an argument and returns the first element that matches that selector. 
+   //Shall be used for performing various operations in the webpage to make it more interactive.
+const button1 = document.querySelector("#button1"); 
+const button2 = document.querySelector("#button2"); 
+const button3 = document.querySelector("#button3"); 
+const text = document.querySelector("#text");
+const xpText = document.querySelector("#xpText");
+const healthText = document.querySelector("#healthText");
+const goldText = document.querySelector("#goldText");
+const monsterStats = document.querySelector("#monsterStats");
+const monsterName = document.querySelector("#monsterName");
+const monsterHealthText = document.querySelector("#monsterHealth");
 
+
+// initialize buttons
+button1.onclick = goStore;
+button2.onclick = goCave;
+button3.onclick = fightDragon;
+
+function update(location) {
+
+}
+
+// go to town after going to store
+function goTown() {
+  button1.innerText = "Go to store";
+  button2.innerText = "Go to cave";
+  button3.innerText = "Fight dragon";
+  button1.onclick = goStore;
+  button2.onclick = goCave;
+  button3.onclick = fightDragon;
+  text.innerText = "You are in the town square. You see a sign that says \"Store\".";
+}
+
+// go to the store
+function goStore() {
+  button1.innerText = "Buy 10 health (10 gold)";
+  button2.innerText = "Buy weapon (30 gold)";
+  button3.innerText = "Go to town square";
+  button1.onclick = buyHealth;
+  button2.onclick = buyWeapon;
+  button3.onclick = goTown;
+  text.innerText = "You enter the store.";
+}
+
+// go to the cave
+function goCave() {
+  console.log("Going to cave.");
+}
+
+// fight the dragon
+function fightDragon() {
+  console.log("Fighting dragon.");
+}
+
+// buy health potion in store 
+function buyHealth() {
+
+}
+
+// buy a weapon in store
+function buyWeapon() {
+
+}
