16e737f092126f910f8672ddc1e3df3fa9c97edf
Anirban Bhattacharya <10656131+obhi1kenobhi@users.noreply.github.com>
2024-03-15 13:20:56 +0530

    added the script file

---
+++ rpg-dragon-repeller/script.js
@@ -0,0 +1,14 @@
+// //Variable declarations are of three types - var, const and let. 'let' is the most popular and shall be used here.
+let xp = 0;  // Declare a variable called xp. Initialize your xp variable to have a value of 0, a number.
+let health = 100; // Initialize another variable called health with a value of 100
+let gold = 50; // Initialize another variable called gold with a value of 50.
+let currentWeapon = 0; // Create another variable called currentWeapon and set it to 0.
+let fighting; // Declare a variable called fighting but do not initialize it with a value.
+let monsterHealth; // Declare a variable called monsterHealth but do not initialize it with a value.
+let inventory = ["stick"]; // Declare an array variable and initialize it to only one element, stick for now.
+
+// //The querySelector() method takes a CSS selector as an argument and returns the first element that matches that selector.
+const button1 = document.querySelector("#button1"); // Create a button1 constant and use querySelector() to assign it your element with the id of button1.
+const button2 = document.querySelector("#button2"); // Create a button2 constant and use querySelector() to assign it your element with the id of button2.
+const button3 = document.querySelector("#button3"); // Create a button3 constant and use querySelector() to assign it your element with the id of button3.
+
