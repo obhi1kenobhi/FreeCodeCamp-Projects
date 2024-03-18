7468bfc8fb316f58faa0eec05534bdf0c9644c00
Anirban Bhattacharya <10656131+obhi1kenobhi@users.noreply.github.com>
2024-03-18 19:51:00 +0530

    added input validation functions to script

--- calorie-counter/scripts/script.js
+++ calorie-counter/scripts/script.js
@@ -6,14 +6,15 @@ const clearButton = document.getElementById('clear');
 const output = document.getElementById('output');
 let isError = false;
 
-// function to check numbers using array
+// function to check numbers using regex
 function cleanInputString(str) {
-  const strArray = str.split('');
-  const cleanStrArray = [];
+    const regex = /[+-\s]/g; // /<exp>/ - regex expression to check; [abcxyz] - wrapper of a character class which searches for any no. of symbols within the class; 
+                             // g after the closing slash will tell the pattern to continue looking even after it has found a match
+    return str.replace(regex,""); // replace function replaces one string/regex expression with another string
+}
 
-  for (let i = 0; i < strArray.length; i++) {
-    if (!["+", "-", " "].includes(strArray[i])) {
-      cleanStrArray.push(strArray[i])
-    }
-  }
+// function to filter out exponential notations
+function isInvalidInput(str) {
+    const regex = /\d+e\d+/i; // i flag is used for insensitive, so both upper and lowercase will be allowed; d flag is used as shorthand for all digits
+    return str.match(regex); // match function matches first instance and returns it, or returns an array of all matches if regex is global
 }
