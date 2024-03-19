b5efc91aae003abbb5648a1e60b6869e3844aa60
Anirban Bhattacharya <10656131+obhi1kenobhi@users.noreply.github.com>
2024-03-19 19:30:55 +0530

    Operable state

--- calorie-counter/scripts/script.js
+++ calorie-counter/scripts/script.js
@@ -32,10 +32,45 @@ function addEntry() {
       min="0"
       id="${entryDropdown.value}-${entryNumber}-calories"
       placeholder="Calories"
-    />`;                        // HTML string which takes entry of the calorie intake
+    />`;                        // HTML string which takes entry of the calorie intake, using the template literal format
     targetInputContainer.insertAdjacentHTML('beforeend', HTMLString); //insertAdjacentHTML function uses two arguments - 1st is the position where to insert the HTML, 
                                                                       // 2nd is to insert the HTML itself
   }
+
+function calculateCalories(e) {
+  e.preventDefault(); // The preventDefault() method cancels the event if it is cancelable, meaning that form data will not be submitted.
+  isError = false;
+
+  const breakfastNumberInputs = document.querySelectorAll('#breakfast input[type=number]');
+  const lunchNumberInputs = document.querySelectorAll('#lunch input[type=number]');
+  const dinnerNumberInputs = document.querySelectorAll('#dinner input[type=number]');
+  const snacksNumberInputs = document.querySelectorAll('#snacks input[type=number]');
+  const exerciseNumberInputs = document.querySelectorAll('#exercise input[type=number]');
+
+  const breakfastCalories = getCaloriesFromInputs(breakfastNumberInputs); 
+  const lunchCalories = getCaloriesFromInputs(lunchNumberInputs);
+  const dinnerCalories = getCaloriesFromInputs(dinnerNumberInputs);
+  const snacksCalories = getCaloriesFromInputs(snacksNumberInputs);
+  const exerciseCalories = getCaloriesFromInputs(exerciseNumberInputs);
+  const budgetCalories = getCaloriesFromInputs([budgetNumberInput]); // since budgetNumberInputs is not a list item in itself, it is made a list by wrapping it in []
+
+  if (isError) { // used to check whether global error flag returns as true - i.e. iinvalid input in budgetNumberInput
+    return;
+  }
+
+  const consumedCalories = breakfastCalories + lunchCalories + dinnerCalories + snacksCalories;
+  const remainingCalories = budgetCalories - consumedCalories + exerciseCalories;
+  const surplusOrDeficit = remainingCalories < 0 ? 'Surplus' : 'Deficit'; // single-line ternary operator which functions as an if-else statement
+  output.innerHTML = `
+  <span class="${surplusOrDeficit.toLowerCase()}">${Math.abs(remainingCalories)} Calorie ${surplusOrDeficit}</span>
+  <hr>
+  <p>${budgetCalories} Calories Budgeted</p>
+  <p>${consumedCalories} Calories Consumed</p>
+  <p>${exerciseCalories} Calories Burned</p>
+  `; // template literal format used here too
+
+  output.classList.remove('hide');
+}
 
 // function to collect all calories from inputs, add them and return them 
 function getCaloriesFromInputs(list) {
@@ -54,4 +89,23 @@ function getCaloriesFromInputs(list) {
   }
   return calories;
 }  
+
+function clearForm() {
+  const inputContainers = Array.from(document.querySelectorAll('.input-container')); // The Array.from() static method creates a new, shallow-copied Array instance from an iterable or array-like object.
+
+
+
+  for (const container of inputContainers) {
+    container.innerHTML = '';
+  }
+
+  budgetNumberInput.value = '';
+  output.innerText = '';
+  output.classList.add('hide'); // adds the 'hide' class in output when the form is cleared
+}
+
+
+
 addEntryButton.addEventListener("click", addEntry); // Event Listener to listen to the click event of addEntryButton
+calorieCounter.addEventListener("submit", calculateCalories);
+clearButton.addEventListener("click",clearForm);
