df5af5376e2a122e548d68593bf082e704f04c21
Anirban Bhattacharya <10656131+obhi1kenobhi@users.noreply.github.com>
2024-05-05 13:18:31 +0530

    added platform collision logic

--- platformer-game/scripts/script.js
+++ platformer-game/scripts/script.js
@@ -1,4 +1,3 @@
-
 const startBtn = document.getElementById("start-btn");
 const canvas = document.getElementById("canvas");
 const startScreen = document.querySelector(".start-screen");
@@ -72,6 +71,8 @@ class Platform {
   }
 }
 
+
+
 const player = new Player();
 
 const platformPositions = [
@@ -121,7 +122,33 @@ const animate = () => {
     }
   }
 
+  platforms.forEach((platform) => {
+    const collisionDetectionRules = [
+      player.position.y + player.height <= platform.position.y,
+      player.position.y + player.height + player.velocity.y >= platform.position.y,
+      player.position.x >= platform.position.x - player.width / 2,
+      player.position.x <=
+        platform.position.x + platform.width - player.width / 3,
+    ];
 
+    if (collisionDetectionRules.every((rule) => rule)) {
+      player.velocity.y = 0;
+      return;
+    }
+
+    const platformDetectionRules = [
+      player.position.x >= platform.position.x - player.width / 2,
+      player.position.x <=
+        platform.position.x + platform.width - player.width / 3,
+      player.position.y + player.height >= platform.position.y,
+      player.position.y <= platform.position.y + platform.height,
+    ];
+
+    if (platformDetectionRules.every(rule => rule)) {
+      player.position.y = platform.position.y + player.height;
+      player.velocity.y = gravity;
+    };
+  });
 }
 
 
